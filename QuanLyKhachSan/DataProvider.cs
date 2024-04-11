c653ba31c8954488ced5c8faba8d36c530570fd6
hungle10 <caylaco1981@gmail.com>
2024-04-11 13:28:35 +0700

    Add connect Hung

--- QuanLyKhachSan/DataProvider.cs
+++ QuanLyKhachSan/DataProvider.cs
@@ -27,6 +27,7 @@ namespace QuanLyKhachSan
        // private string connectionStr = @"Data Source=DESKTOP-D1VEMR6\DINHHOA;Initial Catalog=QLKS;Integrated Security=True";
         //private string connectionStr = @"Data Source=LEHUNG;Initial Catalog=QLCF;Integrated Security=True";
         private string connectionStr = @"Data Source=LAPTOP-7D271VJA\MINHVAN;Initial Catalog=QLKS;Integrated Security=True;TrustServerCertificate=True";
+        //Data Source=LEHUNG\THAIHUNG;Initial Catalog=HeQuanTriCSDL_QuanLyKhachSan;Integrated Security=True;Trust Server Certificate=True
 
         public DataTable ExecuteQuery(string query, object[] parameter = null)
         {
